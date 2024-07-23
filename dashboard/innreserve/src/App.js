b2ff6c182479415246a303cfc5fdf7aa68f5f3fa
Annacletter <anna.cletter12@gmail.com>
2024-07-23 15:02:27 -0700

    amending all errors for deployment

--- dashboard/innreserve/src/App.js
+++ dashboard/innreserve/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import BookingForm from './components/bookings/BookingForm';
+import BookingList from './components/bookings/BookingList';
+import AuthState from './context/AuthState';
+import BookingState from './context/BookingState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+
+const App = () => {
+  return (
+    <AuthState>
+      <BookingState>
+        <Router>
+          <Navbar />
+          <section className="container">
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/bookings/new" element={<BookingForm />} />
+              <Route path="/bookings" element={<BookingList />} />
+            </Routes>
+          </section>
+        </Router>
+      </BookingState>
+    </AuthState>
+  );
+};
 
 export default App;
